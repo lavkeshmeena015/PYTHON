b2236d43f7cfd249a55e14c74570e9980692e113
Lavkesh Meena <lavkeshmeena015@gmail.com>
2024-01-19 22:57:19 +0530

    basics

---
+++ day1.py
@@ -0,0 +1,27 @@
+# to display anything on console
+print("hello everyone")
+
+# variable 
+a = "lovee" #storing string in variable
+b = 25 #storing int in variable
+c = 10.5 #storing string in variable
+
+print(a)
+print(b)
+print(c)
+
+# taking input and display it to console
+
+d = input("enter your input : ")
+print(d)
+
+# to display multiline string
+
+print('''
+    hii how are you
+    i am fine 
+    what about you  
+    ''')
+
+# to print a variable's numerical value with string
+print("this is a variable c's value : " + str(b))
